01b7f4ff23999ee3af121f6f2d0bcf2c477f0f59
Daniel Hirsch <daniel.hirsch.garcia@gmail.com>
2025-01-09 18:25:58 +0100

    feat: inici arxiu

--- v1/src/main/java/com/project/Main.java
+++ v1/src/main/java/com/project/Main.java
@@ -1,7 +1,12 @@
 package com.project;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import org.json.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    String filePathUsuaris = "./data/usuaris.json";
+
+    List<Map<String, 
 }
