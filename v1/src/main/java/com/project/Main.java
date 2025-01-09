75daf868517d03e1233c6493c1c44593b0b2d435
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2025-01-09 18:16:31 +0100

    feat: Creació dels menus de llibres i navegació entre menus

--- v1/src/main/java/com/project/Main.java
+++ v1/src/main/java/com/project/Main.java
@@ -1,6 +1,83 @@
 package com.project;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
+
+    public static ArrayList<String> menuLlibres() {
+        String menuText = """
+                Gestió de llibres
+                1. Afegir
+                2. Modificar
+                3. Eliminar
+                4. Llistar
+                0. Tornar al menú principal
+                """;
+        String[] lines = menuText.split("\\R");
+        return new ArrayList<>(Arrays.asList(lines));
+    }
+
+    public static ArrayList<String> menuLlistarLlibres() {
+        String menuText = """
+                Llistar llibres
+                1. Tots
+                2. En préstec
+                3. Per autor
+                4. Cercar títol
+                0. Tornar al menú de llibres
+                """;
+        String[] lines = menuText.split("\\R");
+        return new ArrayList<>(Arrays.asList(lines));
+    }
+
+    public static String obtenerOpcion(Scanner scanner) {
+        ArrayList<String> menu = menuLlibres();
+
+        while (true) {
+            System.out.print("Escull una opció: ");
+            String opcio = scanner.nextLine();
+
+            try {
+                int index = Integer.parseInt(opcio);
+                if (index == 0) {
+                    return "Tornar al menú principal";
+                }
+                else if (index > 0 && index < menu.size() - 1) {
+                    return menu.get(index).substring(3).trim();
+                }
+            }
+            catch (NumberFormatException e) {
+            }
+
+            System.out.println("Opció no vàlida. Torna a intentar-ho");
+        }
+    }
+
+    public static String obtenerOpcionLlistar(Scanner scanner) {
+        ArrayList<String> menu = menuLlistarLlibres();
+
+        while (true) {
+            System.out.print("Escull una opció: ");
+            String opcio = scanner.nextLine();
+
+            try {
+                int index = Integer.parseInt(opcio);
+                if (index == 0) {
+                    return "Tornar al menú de llibres";
+                }
+                else if (index > 0 && index < menu.size() - 1) {
+                    return menu.get(index).substring(3).trim();
+                }
+            }
+            catch (NumberFormatException e) {
+            }
+
+            System.out.println("Opció no vàlida. Torna a intentar-ho");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
