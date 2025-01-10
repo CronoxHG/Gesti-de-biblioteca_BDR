ad948a1b3a8a90916d7c01d5df41c57808e891c5
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-01-10 21:08:14 +0100

    fix : verificar que l'usuari i el llibre no estiguin prestats dintre del prestecs.json

--- v1/src/main/java/com/project/Main.java
+++ v1/src/main/java/com/project/Main.java
@@ -4,23 +4,76 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+import javax.management.RuntimeErrorException;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Main {
-    public static boolean afegir_prestec() {
+    public static void afegir_prestec() {
+        Scanner scanner = new Scanner(System.in);
         String filePath = "./data/prestecs.json";
         String content = new String(Files.readAllBytes(Paths.get(filePath)));
         JSONArray prestecs = new JSONArray(content);
+        // no puc preguntar l'id del prèstec hauria de ser autoincrement.
+        System.out.print("Introdueix l'ID del llibre que vols: ");// podriem fer-lo també amb títol.
+        String IdLlibre = scanner.nextLine();
 
-        content.getJSONObject("0");
-    if (prestecs.has("IdLlibre")){// existe el libro
+        // verificar que el idLlibre es un número.
+        try {
+            Integer.parseInt(IdLlibre);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: " + IdLlibre + "no és un id vàlid.");
+            return;
+        }
 
-        System.out.print("");
-        prestecs.put("nom", nomLlibre)
+        Integer idLlibreInteger = Integer.parseInt(IdLlibre);
+        for (int i = 0; i < prestecs.length(); i++) {
+            JSONObject prestec = prestecs.getJSONObject(i);
+            // si el libro está prestado no puedo prestarlo.
+            if (prestec.getInt("IdLlibre") == idLlibreInteger) {
+                try {
+                    throw new RuntimeException();
+                } catch (RuntimeException e) {
+                    System.out.println("El llibre amb id " + idLlibreInteger + " ja està prestat.");
+                    return;
+                }
 
+                // també es podría fer així:
+                // System.out.println("El llibre amb id "+idLlibreInteger+" ja està prestat.");
+                // return;
+            }
+        }
+
+        System.out.print("Introdueix l'ID del usuari: ");// podriem fer-lo també amb títol.
+        String idUsuari = scanner.nextLine();
+        try {
+            Integer.parseInt(idUsuari);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: " + idUsuari + "no és un id d'usuari vàlid.");
+            return;
+        }
+        Integer idUsuariInteger = Integer.parseInt(idUsuari);
+
+        // si l'usuari té 4 llibres prestats, no pot més.
+        Integer cnt = 0;
+        for (int i = 0; i < prestecs.length(); i++) {
+            JSONObject prestec = prestecs.getJSONObject(i);
+            if (prestec.getInt("IdUsuari") == idUsuariInteger){
+                cnt += 1;                
+            }
+        }
+        if (cnt>=4){
+            try{
+                throw new RuntimeException();
+            } catch (RuntimeException e){
+                System.out.println("El llibre amb id " + idLlibreInteger + " ja està prestat.");
+                return;
+            }
+        }
     }
-}
+
+    // verificar que existeixin el llibre i l'usuari.
 
     public static String gestio_prestecs() {
         Scanner scanner = new Scanner(System.in);
