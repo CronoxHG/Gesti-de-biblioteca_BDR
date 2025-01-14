7a02ce9e203c7497e99e17002a4822bcca0e4210
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2025-01-15 00:18:31 +0100

    feat: Creació del menu principal

--- v1/src/main/java/com/project/Main.java
+++ v1/src/main/java/com/project/Main.java
@@ -1,7 +1,80 @@
 package com.project;
+import java.util.ArrayList;
+
+import java.util.*;
 
 public class Main {
+
+    public static void dibuixarLlista() {
+        
+    }
+
+    public static ArrayList<String> menuPrincipal() {
+        String menuText = """
+                Gestió de biblioteca
+                1. Llibres
+                2. Usuaris
+                3. Préstecs
+                0. Sortir
+                """;
+        String[] lines = menuText.split("\\R");
+        return new ArrayList<>(Arrays.asList(lines));
+    }
+
+    public static String obtenerOpcion(Scanner scanner) {
+        ArrayList<String> menu = menuPrincipal();
+    
+        while (true) {
+            System.out.print("Escull una opció: ");
+            String opcio = scanner.nextLine();
+    
+            try {
+                int index = Integer.parseInt(opcio);
+                if (index == 0) {
+                    return "Sortir";
+                }
+                else if (index > 0 && index < menu.size() - 1) {
+                    return menu.get(index).substring(3).trim();
+                }
+            }
+            catch (NumberFormatException e) {
+                // Ignorar la excepción y pedir otra entrada
+            }
+    
+            System.out.println("Opció no vàlida. Torna a intentar-ho");
+        }
+    }
+
+    public static void gestionaMenuPrincipal(Scanner scanner) {
+        ArrayList<String> menuLlibres = menuPrincipal();
+    
+        while (true) {
+            clearScreen();
+            dibuixarLlista(menuLlibres);
+    
+            String opcio = obtenerOpcion(scanner);
+            switch (opcio) {
+                case "Tornar al menú principal":
+                    return;
+                case "Llibres":
+                    // Aquí se llama a la funcion para ir a Libros
+                    break;
+                case "Usuaris":
+                    // Aquí se llama a la funcion para ir a Usuarios
+                    break;
+                case "Préstecs":
+                    // Aquí se llama a la función para ir a prestamos
+                    break;
+                default:
+                    System.out.println("Opció no vàlida. Torna a intentar-ho");
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        gestionaMenuPrincipal(scanner);
+
+        scanner.close();
     }
 }
